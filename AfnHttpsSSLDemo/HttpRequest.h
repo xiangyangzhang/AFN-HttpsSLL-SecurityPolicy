d0f18dde298ea9517186cebc7e6903a0e941a010
xiangyangzhang <xiangyanga23@163.com>
2016-04-18 13:45:38 +0800

    f

---
+++ AfnHttpsSSLDemo/HttpRequest.h
@@ -0,0 +1,13 @@
+//
+//  HttpRequest.h
+//  AfnHttpsSSLDemo
+//
+//  Created by Apple on 16/1/20.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpRequest : NSObject
+
+@end
---
+++ AfnHttpsSSLDemo/HttpRequest.m
@@ -0,0 +1,13 @@
+//
+//  HttpRequest.m
+//  AfnHttpsSSLDemo
+//
+//  Created by Apple on 16/1/20.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import "HttpRequest.h"
+
+@implementation HttpRequest
+
+@end
